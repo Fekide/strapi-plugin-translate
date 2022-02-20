d2e8c681b6aef944dd0355015b8b55d40d3180d3
Felix Haase <felix.haase@feki.de>
2022-02-20 14:36:36 +0100

    validation for config

--- server/config/index.js
+++ server/config/index.js
@@ -15,5 +15,23 @@ module.exports = {
     ],
     glossaryId: null,
   }),
-  validator() {},
+  validator({ apiKey, translatedFieldTypes, freeApi, glossaryId }) {
+    if (!apiKey) {
+      throw new Error(
+        'apiKey is not set, disable the plugin if you do not have one since the translations will not succeed'
+      )
+    }
+    if (typeof apiKey != 'string') {
+      throw new Error('apiKey must be a string')
+    }
+    if (!Array.isArray(translatedFieldTypes)) {
+      throw new Error('translatedFieldTypes has to bee an array')
+    }
+    if (typeof freeApi !== 'boolean') {
+      throw new Error('freeApi has to bee a boolean')
+    }
+    if (glossaryId !== null && typeof freeApi !== 'string') {
+      throw new Error('glossaryId should be a string if it is defined')
+    }
+  },
 }
