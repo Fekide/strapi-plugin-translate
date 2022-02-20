4eeacc1a7d62f1353c1f12b5e4a68b1b7ef52cac
Felix Haase <felix.haase@feki.de>
2022-02-20 20:12:33 +0100

    Cleanup currently unused settings section

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,35 +2,12 @@ import { prefixPluginTranslations } from '@strapi/helper-plugin'
 import pluginPkg from '../../package.json'
 import pluginId from './pluginId'
 import Initializer from './components/Initializer'
-import PluginIcon from './components/PluginIcon'
 import CMEditViewTranslateLocale from './components/CMEditViewTranslateLocale'
 
 const name = pluginPkg.strapi.name
 
 export default {
   register(app) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import(
-          /* webpackChunkName: "[request]" */ './pages/App'
-        )
-
-        return component
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    })
     app.registerPlugin({
       id: pluginId,
       initializer: Initializer,
