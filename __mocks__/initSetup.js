70728ed35447fdd3aec4319c6fb93a8031d38d9d
Felix Haase <felix.haase@feki.de>
2022-04-14 15:25:50 +0200

    add entityService to mocks

--- __mocks__/initSetup.js
+++ __mocks__/initSetup.js
@@ -9,6 +9,7 @@ module.exports = ({
   // contentTypes should not be in the actual format but the formatted version
   contentTypes = {},
   plugins = {},
+  db = {},
 }) => {
   const dbConfig = toStore
     ? {
@@ -26,9 +27,9 @@ module.exports = ({
         const [handler, collection] = uid.split('::')
         const values = get(mock.db, `${handler}.${collection}.records`, [])
         return {
-          findOne: async (id) =>
+          findOne: async ({ where }) =>
             new Promise((resolve) =>
-              resolve(values.find((obj) => obj.id == id))
+              resolve(values.find((obj) => obj.id == where.id.$eq))
             ),
           findMany: async () => new Promise((resolve) => resolve(values)),
           findWithCount: async () =>
@@ -37,6 +38,7 @@ module.exports = ({
           create: async (value) => new Promise((resolve) => resolve(value)),
           update: async (value) => new Promise((resolve) => resolve(value)),
           delete: async (value) => new Promise((resolve) => resolve(value)),
+          ...db.query?.(uid, { strapi, values }),
         }
       },
       ...dbConfig,
@@ -77,6 +79,7 @@ module.exports = ({
       return new Promise((resolve) => resolve(mockedStore))
     },
     plugins: {
+      ...plugins,
       deepl: {
         service: function (name) {
           return this.services[name]({ strapi: mock.getRef() })
@@ -88,12 +91,26 @@ module.exports = ({
         services: {
           deepl: require('../server/services/deepl'),
           translate: require('../server/services/translate'),
+          'batch-translate-job': () => {
+            const uid = 'plugin::deepl.batch-translate-job'
+            return {
+              findOne: this.db.query(uid).findOne,
+              find: this.db.query(uid).findMany,
+              count: this.db.query(uid).count,
+              create: this.db.query(uid).create,
+              update: this.db.query(uid).update,
+              delete: this.db.query(uid).delete,
+            }
+          },
         },
         controllers: {
           deepl: require('../server/controllers/deepl'),
           translate: require('../server/controllers/translate'),
+          'batch-translate-job': require('../server/controllers/batch-translate-job'),
         },
-        contentTypes: {},
+        contentTypes: {
+          'batch-translate-job': require('../server/content-types/batch-translate-job/schema.json'),
+        },
       },
       'content-type-builder': {
         service: function (name) {
@@ -113,7 +130,6 @@ module.exports = ({
           },
         },
       },
-      ...plugins,
     },
     config: {
       get: function (prop = '') {
@@ -139,6 +155,15 @@ module.exports = ({
         },
       },
     },
+    entityService: {
+      findOne: (uid, id, params) =>
+        mock.db.query(uid).findOne({ where: { id: { $eq: id } }, ...params }),
+      findMany: (uid, params) =>
+        mock.db.query(uid).findMany({ ...params, where: params.filters }),
+      create: (uid, params) => mock.db.query(uid).create(params),
+      update: (uid, id, params) => mock.db.query(uid).update(id, params),
+      delete: (uid, id, params) => mock.db.query(uid).delete(id, params),
+    },
     service(uid) {
       const [handler, collection] = uid.split('::')
       if (handler === 'plugin') {
@@ -146,12 +171,11 @@ module.exports = ({
         return this.plugins[plugin].services[service]({ strapi: mock.getRef() })
       }
       return {
-        findOne: this.db.query(uid).findOne,
-        find: this.db.query(uid).findMany,
-        count: this.db.query(uid).count,
-        create: this.db.query(uid).create,
-        update: this.db.query(uid).update,
-        delete: this.db.query(uid).delete,
+        findOne: (id, params) => this.entityService.findOne(uid, id, params),
+        find: (params) => this.entityService.findMany(uid, params),
+        create: (params) => this.entityService.create(uid, params),
+        update: (id, params) => this.entityService.update(uid, id, params),
+        delete: (id, params) => this.entityService.delete(uid, id, params),
       }
     },
     log: {
