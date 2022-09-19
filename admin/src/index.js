542d8994f8b1cc8e2c6111db6f18e4d9bd030852
Felix Haase <felix.haase@feki.de>
2022-09-19 20:18:14 +0200

    Add frontend settings for field specific translation

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,9 +1,13 @@
+import get from 'lodash/get'
 import { prefixPluginTranslations } from '@strapi/helper-plugin'
 import pluginPkg from '../../package.json'
 import pluginId from './pluginId'
 import Initializer from './components/Initializer'
 import CMEditViewTranslateLocale from './components/CMEditViewTranslateLocale'
 import PluginIcon from './components/PluginIcon'
+import mutateCTBContentTypeSchema from './utils/mutateCTBContentTypeSchema'
+import TRANSLATABLE_FIELDS from './utils/translatableFields'
+import { getTrad } from './utils'
 
 const name = pluginPkg.strapi.name
 
@@ -42,6 +46,113 @@ export default {
       name: 'deepl-locale-translate',
       Component: CMEditViewTranslateLocale,
     })
+
+    const ctbPlugin = app.getPlugin('content-type-builder')
+
+    if (ctbPlugin) {
+      const ctbFormsAPI = ctbPlugin.apis.forms
+      ctbFormsAPI.addContentTypeSchemaMutation(mutateCTBContentTypeSchema)
+
+      ctbFormsAPI.extendFields(TRANSLATABLE_FIELDS, {
+        validator: () => {},
+        form: {
+          advanced({
+            contentTypeSchema,
+            forTarget,
+            type,
+            step,
+            data,
+            ...rest
+          }) {
+            if (forTarget === 'contentType') {
+              const contentTypeHasI18nEnabled = get(
+                contentTypeSchema,
+                ['schema', 'pluginOptions', 'i18n', 'localized'],
+                false
+              )
+              const attributeHasi18nEnabled = get(
+                data,
+                ['pluginOptions', 'i18n', 'localized'],
+                false
+              )
+              const attributeType = data.type
+              if (
+                !contentTypeHasI18nEnabled ||
+                (attributeType !== 'relation' && !attributeHasi18nEnabled)
+              ) {
+                return []
+              }
+            }
+
+            // if (type === 'component' && step === '1') {
+            //   return []
+            // }
+
+            return [
+              {
+                name: 'pluginOptions.deepl.translate',
+                type: 'select',
+                intlLabel: {
+                  id: getTrad('content-type-builder.form.label'),
+                  defaultMessage:
+                    'Configure automated translation for this field?',
+                },
+                description: {
+                  id: getTrad('content-type-builder.form.description'),
+                  defaultMessage:
+                    'How should the DeepL plugin handle the translation of this field?',
+                },
+                validations: {},
+                options: [
+                  {
+                    key: '__null_reset_value__',
+                    value: '',
+                    metadatas: {
+                      intlLabel: {
+                        id: 'components.InputSelect.option.placeholder',
+                        defaultMessage: 'Choose here',
+                      },
+                    },
+                  },
+                  {
+                    key: 'translate',
+                    value: 'translate',
+                    metadatas: {
+                      intlLabel: {
+                        id: getTrad(
+                          'content-type-builder.form.value.translate'
+                        ),
+                        defaultMessage: 'Translate',
+                      },
+                    },
+                  },
+                  {
+                    key: 'copy',
+                    value: 'copy',
+                    metadatas: {
+                      intlLabel: {
+                        id: getTrad('content-type-builder.form.value.copy'),
+                        defaultMessage: 'Copy',
+                      },
+                    },
+                  },
+                  {
+                    key: 'delete',
+                    value: 'delete',
+                    metadatas: {
+                      intlLabel: {
+                        id: getTrad('content-type-builder.form.value.delete'),
+                        defaultMessage: 'Delete',
+                      },
+                    },
+                  },
+                ],
+              },
+            ]
+          },
+        },
+      })
+    }
   },
   async registerTrads({ locales }) {
     const importedTrads = await Promise.all(
