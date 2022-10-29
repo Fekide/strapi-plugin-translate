383cd59639ff9cd22dbe088ec3532100c05e95b3
Felix Haase <felix.haase@feki.de>
2022-10-29 14:51:02 +0200

    chore(playground): add playground for testing
    The playgrond can be used to evaluate changes and also possibly in the future do e2e tests

---
+++ playground/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...
+  translate: {
+    enabled: true,
+    config: {
+      provider: 'deepl',
+      providerOptions: {
+        apiKey: '6879ca17-c844-af0c-0a36-667f340e19e5:fx',
+      },
+    },
+  },
+  // ...
+}
