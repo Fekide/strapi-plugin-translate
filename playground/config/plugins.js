5f0f793e511e0772292b24c626c550a1cd4b6ff3
Felix Haase <felix.haase@feki.de>
2022-11-01 16:11:09 +0100

    chore: remove token

--- playground/config/plugins.js
+++ playground/config/plugins.js
@@ -1,13 +1,11 @@
-module.exports = {
+module.exports = ({ env }) => ({
   // ...
   translate: {
     enabled: true,
     config: {
       provider: 'deepl',
-      providerOptions: {
-        apiKey: '6879ca17-c844-af0c-0a36-667f340e19e5:fx',
-      },
+      providerOptions: {},
     },
   },
   // ...
-}
+})
