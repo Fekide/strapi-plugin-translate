bf74fdf2fc94fd1009ad5b62e36052abb21c50d0
Felix Haase <felix.haase@feki.de>
2022-11-12 13:05:40 +0100

    chore(test): run tests in each workspace separately

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  // setupFilesAfterEnv: ['<rootDir>/test/unit.setup.js'],
-  transform: {},
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  // collectCoverageFrom: ['./admin/src/**/*.(js|jsx)', './server/**/*.(js|jsx)'],
   testMatch: ['**/__tests__/?(*.)+(spec|test).js'],
 }
