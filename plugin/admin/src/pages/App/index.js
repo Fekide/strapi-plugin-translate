e94dd9e8a8ecb9f3790365613cc5430155dd5fdb
Felix Haase <felix.haase@feki.de>
2023-11-02 22:37:41 +0100

    refactor(admin): remove unnecessary react router switch

--- plugin/admin/src/pages/App/index.js
+++ plugin/admin/src/pages/App/index.js
@@ -6,16 +6,12 @@
  */
 
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import pluginId from '../../pluginId'
 import HomePage from '../HomePage'
 
 const App = () => {
   return (
     <div>
-      <Switch>
-        <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
-      </Switch>
+      <HomePage />
     </div>
   )
 }
