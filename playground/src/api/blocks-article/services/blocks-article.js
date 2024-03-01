3caa95e1e21a220612887e1069a3669687cd2dbe
Felix Haase <felix.haase@feki.de>
2024-03-01 22:41:15 +0100

    chore(playground): add article component with blocks element
    Adds a new content type with the new blocks rich text format

---
+++ playground/src/api/blocks-article/services/blocks-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blocks-article service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::blocks-article.blocks-article');
