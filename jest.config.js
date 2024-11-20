005feac99eb7f3a32380a8be6348954bbae2bb2d
Felix Haase <felix.haase@feki.de>
2024-11-20 16:42:24 +0100

    test: remove comment

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   projects: [
     '<rootDir>/plugin/jest.config.js',
     '<rootDir>/providers/*/jest.config.js',
-    // '<rootDir>/.github/actions/*/jest.config.js',
   ],
   reporters: [
     'default',
