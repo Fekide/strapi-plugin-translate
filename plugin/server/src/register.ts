4af6545ff70a00e857178994cf3ebcda4fb6bc87
zack <zavven.z@gmail.com>
2025-04-01 22:06:23 +0800

    feat: support documentation plugin

--- plugin/server/src/register.ts
+++ plugin/server/src/register.ts
@@ -1,7 +1,17 @@
 import type { Core } from '@strapi/strapi'
+import spec from '../../documentation/content-api.json';
 
 const register = ({ strapi }: { strapi: Core.Strapi }) => {
   // register phase
+  if (strapi.plugin('documentation')) {
+    strapi
+      .plugin('documentation')
+      .service('override')
+      .registerOverride(spec, {
+        pluginOrigin: 'translate',
+        excludeFromGeneration: ['translate'],
+      });
+  }
 }
 
 export default register
